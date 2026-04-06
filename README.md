7dace3c90f893e7827d9074cfd8a47ac06c52b9a
sudeepreddy143 <sudeeppanyam@gmail.com>
2026-04-06 12:01:23 -0500

    added initial app files and content

---
+++ README.md
@@ -0,0 +1,6 @@
+\# Student Profile App
+
+
+
+This is a beginner project created while learning Git and GitHub using Command Prompt.
+
---
+++ script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    document.getElementById("message").innerText = "Welcome to Git and GitHub learning!";
+}
